6861bbcda04ea8fb7f9c762315c09a2bdc832e57
Stereowalker <stereowalker.mc@gmail.com>
2021-06-11 12:37:16 +0100

    Added resistances for different elemental magic

--- src/main/java/com/stereowalker/combat/entity/ai/CAttributes.java
+++ src/main/java/com/stereowalker/combat/entity/ai/CAttributes.java
@@ -18,6 +18,11 @@ public class CAttributes {
 	public static final Attribute HEALTH_REGENERATION = register("generic.health_regeneration", (new RangedAttribute("attribute.name.combat.health_regeneration", 1.0D, 0.0D, 1024.0D)).setShouldWatch(true));
 	public static final Attribute JUMP_STRENGTH = register("generic.jump_strength", (new RangedAttribute("attribute.name.combat.jump_strength", 0.2D, -1024.0D, 1024.0D)).setShouldWatch(true));
 	public static final Attribute PHYSICAL_RESISTANCE = register("generic.physical_resistance", (new RangedAttribute("attribute.name.combat.physical_resistance", 0.0D, 0.0D, 1024.0D)).setShouldWatch(true));
+	public static final Attribute FIRE_RESISTANCE = register("generic.fire_resistance", (new RangedAttribute("attribute.name.combat.fire_resistance", 0.0D, 0.0D, 1024.0D)).setShouldWatch(true));
+	public static final Attribute WATER_RESISTANCE = register("generic.water_resistance", (new RangedAttribute("attribute.name.combat.water_resistance", 0.0D, 0.0D, 1024.0D)).setShouldWatch(true));
+	public static final Attribute EARTH_RESISTANCE = register("generic.earth_resistance", (new RangedAttribute("attribute.name.combat.earth_resistance", 0.0D, 0.0D, 1024.0D)).setShouldWatch(true));
+	public static final Attribute LIGHTNING_RESISTANCE = register("generic.lightning_resistance", (new RangedAttribute("attribute.name.combat.lightning_resistance", 0.0D, 0.0D, 1024.0D)).setShouldWatch(true));
+	public static final Attribute WIND_RESISTANCE = register("generic.wind_resistance", (new RangedAttribute("attribute.name.combat.wind_resistance", 0.0D, 0.0D, 1024.0D)).setShouldWatch(true));
 	public static final Attribute ATTACK_REACH = register("generic.attack_reach", (new RangedAttribute("attribute.name.combat.attack_reach", 2.5D, 0.0D, 1024.0D)).setShouldWatch(true));
 	
 	public static void registerAll(IForgeRegistry<Attribute> registry) {
