e5e85562e2cd9f00c8de65117bb60e0a739385e0
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2022-06-20 18:40:21 +0100

    Reduced the lava level of the Acrotlest dimension by 54 blocks

--- src/main/java/com/stereowalker/combat/mixins/NoiseBasedChunkGeneratorMixin.java
+++ src/main/java/com/stereowalker/combat/mixins/NoiseBasedChunkGeneratorMixin.java
@@ -16,6 +16,8 @@ import com.stereowalker.combat.world.level.block.CBlocks;
 //import com.stereowalker.combat.world.level.levelgen.AcrotlestLayeredBaseStoneSource;
 import com.stereowalker.combat.world.level.levelgen.CNoiseGeneratorSettings;
 
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
 import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.biome.BiomeSource;
@@ -25,9 +27,10 @@ import net.minecraft.world.level.levelgen.Aquifer;
 //import net.minecraft.world.level.levelgen.BaseStoneSource;
 import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
 import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
+import net.minecraft.world.level.levelgen.NoiseSettings;
 //import net.minecraft.world.level.levelgen.NoiseSampler;
 //import net.minecraft.world.level.levelgen.NoodleCavifier;
-import net.minecraft.world.level.levelgen.OreVeinifier;
+import net.minecraft.world.level.levelgen.structure.StructureSet;
 import net.minecraft.world.level.levelgen.synth.NormalNoise;
 //import net.minecraft.world.level.levelgen.synth.SurfaceNoise;
 
@@ -48,16 +51,23 @@ public class NoiseBasedChunkGeneratorMixin {
 //	@Shadow @Final protected Supplier<NoiseGeneratorSettings> settings;
 //	@Shadow @Final protected int height;
 //	@Shadow @Final protected NoiseSampler sampler;
-//	@Shadow protected BaseStoneSource baseStoneSource;
 //	@Shadow @Final protected OreVeinifier oreVeinifier;
 //	@Shadow @Final protected NoodleCavifier noodleCavifier;
-//	@Inject(method = "<init>", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
-//	public void init_inject(BiomeSource p_64337_, long p_64338_, Supplier<NoiseGeneratorSettings> p_64339_, CallbackInfo ci) {
-//		NoiseGeneratorSettings noisegeneratorsettings = p_64339_.get();
-//		if (BuiltinRegistries.NOISE_GENERATOR_SETTINGS.getKey(noisegeneratorsettings).equals(CNoiseGeneratorSettings.ACROTLEST.location())) {
-//			this.baseStoneSource = new AcrotlestLayeredBaseStoneSource(p_64338_, this.defaultBlock, CBlocks.SLYAPHY.defaultBlockState(), Blocks.GREEN_TERRACOTTA.defaultBlockState());
-//		}
-//	}
+	@Shadow @Final private Aquifer.FluidPicker globalFluidPicker;
+	@Inject(method = "<init>", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
+	public void init_inject(Registry<StructureSet> p_209106_, Registry<NormalNoise.NoiseParameters> p_209107_, BiomeSource p_209108_, long p_209109_, Holder<NoiseGeneratorSettings> p_209110_, CallbackInfo ci) {
+		NoiseGeneratorSettings noisegeneratorsettings = p_209110_.value();
+		NoiseSettings noisesettings = noisegeneratorsettings.noiseSettings();
+		if (p_209110_.is(CNoiseGeneratorSettings.ACROTLEST.location())) {
+			Aquifer.FluidStatus aquifer$fluidstatus = new Aquifer.FluidStatus(-108, Blocks.LAVA.defaultBlockState());
+			int i = noisegeneratorsettings.seaLevel();
+			Aquifer.FluidStatus aquifer$fluidstatus1 = new Aquifer.FluidStatus(i, noisegeneratorsettings.defaultFluid());
+			Aquifer.FluidStatus aquifer$fluidstatus2 = new Aquifer.FluidStatus(noisesettings.minY() - 1, Blocks.AIR.defaultBlockState());
+			this.globalFluidPicker = (p_198228_, p_198229_, p_198230_) -> {
+				return p_198229_ < Math.min(-108, i) ? aquifer$fluidstatus : aquifer$fluidstatus1;
+			};
+		}
+	}
 //	@Inject(method = "getAquifer", at = @At("HEAD"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
 //	public void init_inject(int pMinCellY, int pCellCountY, ChunkPos pChunkPos, CallbackInfoReturnable<Aquifer> cir) {
 //		if (BuiltinRegistries.NOISE_GENERATOR_SETTINGS.getKey(this.settings.get()).equals(CNoiseGeneratorSettings.ACROTLEST.location())) {
